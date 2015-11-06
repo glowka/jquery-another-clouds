1c0f71d7cc5dbf8b1ad8a02cb376221b4b44d08e
Tomek Główka <tk.glowka@gmail.com>
2015-11-07 00:58:48 +0100

    CloudClick default value updated. JSDoc documentation.

--- jquery.another-clouds.js
+++ jquery.another-clouds.js
@@ -1,67 +1,42 @@
 // by Tomasz Glowka
-// jquery-clouds 0.1
+// jquery-clouds 0.1.1
+
+/**
+ * @overview Tool for creating kind of popovers - clouds.
+ */
+
+/**
+ * jQuery Framework
+ * We use it and extend it by adding jQuery clouds interface.
+ *
+ * @external jQuery
+ */
+
+/**
+ * DOM Element.
+ * After creating Cloud wrap over Element instance, some attributes are set.
+ *
+ * @property {Cloud} cloudApi Reference to Cloud instance created for this element in side-effect manner.
+ * @property {Cloud} data-cloud Data-style reference to Cloud instance created for this element in side-effect
+ *
+ * @external Element
+ */
+
 
 (function($){
 
-jQuery.fn.extend({
-    cloud: function(cmd, options) {
-        options = typeof options !== 'undefined' ? options : {};
-        var $this = this;
-        if(!$this.length)
-            return $this;
-
-        var eachExecute;
-        switch(cmd) {
-            case 'create':
-                eachExecute = function(index, element){
-                    Cloud(element, options);
-                };
-                break;
-            case 'destroy':
-                eachExecute = function(index, element){
-                    if(jQuery(element).data(dataKey)) {
-                        jQuery(element).data(dataKey).destroy();
-                    }
-                };
-                break;
-            default:
-                eachExecute = function(){};
-                break;
-        }
-
-        $this.each(eachExecute);
-        return $this;
-    }
-});
-
-jQuery.extend({
-    cloud: function(cmd, params){
-        params = typeof params !== 'undefined' ? params : null;
-
-        if(typeof cmd === 'string') {
-            switch (cmd) {
-                case 'skins':
-                    return Cloud.skins;
-                default:
-                    break;
-            }
-        }
-        return this;
-    }
-});
-
 var defaults = {
     skin: null,
-    stickTo: null,  // by default stick to previous element
-    positionTo: null,  // position to offset parent of stickTo element
+    stickTo: null,  // By default stick to previous element
+    positionTo: null,  // Position to offset parent of stickTo element
     orientation: 'top-center',  // bottom|top|auto-left|right|center|auto or auto==auto-center
-    distance: [0, 0], // [x, y] //distance to stickTo element
-    positionManually: false,
+    distance: [0, 0], // [x, y] - distance to stickTo element
+    stickManually: false,
     containerStyle: {
         'z-index': 99999
     },
     containerExtraClass: '',
-    cloudClick: function(element, api, event) { event.stopPropagation(); },
+    cloudClick: function(element, api, event) {},
     bgClick: function(element, api, event) { api.close()}
 };
 
@@ -109,13 +84,38 @@ var dataKey = 'cloud',
     openClouds = {},
     skins = {};
 
+    /**
+     * Create cloud wrapper around given node.
+     * @constructs Cloud
+     *
+     * @param {Element|jQuery|Selector} element The wrapped node.
+     * manner.
+     * @param {Object} [options]
+     * @param {String} [options.skin] Name of skin defined using {@link Cloud.skins.set}.
+     * @param {String} [options.stickTo=`DOM previous node`]
+     * @param {Element|jQuery|Selector} [options.positionTo=`DOM offset parent of options.stickTo`]
+     * Cloud will be positioned to this element using it as offset parent.
+     * @param {String} [options.orientation='top-center']
+     * Possible values: bottom|top|auto-left|right|center|auto or auto==auto-center.
+     * @param {Array} [options.distance=[0, 0]] Distance to stickTo element given as [x, y].
+     * @param {Boolean} [options.stickManually=false] Stick manually means, cloud is left as it is.
+     * Stick to stickTo element is abandon and no auto positioning takes place.
+     * @param {Object} [options.containerStyle={'z-index': 99999}] Extra style to be set at cloud container.
+     * @param {String} [options.containerExtraClass=''] Extra class to be appended to main cloud container.
+     * @param {function} [options.cloudClick=function(element, api, event) {}]
+     * Hook and default action for all clicks inside cloud.
+     * @param {function} [options.bgClick=function(element, api, event) { api.close()}]
+     * Hook and default action for all clicks at the backgruond (outside cloud).
+     *
+     * @returns {Cloud}
+     */
 var Cloud = function(element, options){
     // If Cloud object for this element already exists, simply return it
     if(element[fieldKey])
         return element[fieldKey];
 
     var me = {
-        // init & destroy
+        // Init & destroy
         init: function(element, options) {
             for(var f in initial) {if(initial.hasOwnProperty(f)) me[f] = initial[f]}
 
@@ -133,7 +133,8 @@ var Cloud = function(element, options){
             me.elementPrev = me.$elementPrev.get(0);
             me.$stickTo = (!me.options.stickTo) ? me.$element.prev() : jQuery(me.options.stickTo).first();
             me.stickTo = me.$stickTo.get(0);
-            me.$positionTo = (!me.options.positionTo) ? me.$stickTo.offsetParent() : jQuery(me.options.positionTo).first();
+            me.$positionTo = (!me.options.positionTo) ?
+                me.$stickTo.offsetParent() : jQuery(me.options.positionTo).first();
             me.positionTo = me.$positionTo.get(0);
 
             me.createContainer();
@@ -146,7 +147,7 @@ var Cloud = function(element, options){
                 throw 'No mathing stickTo element'
         },
         createContainer: function() {
-            // create container
+            // Create container
             me.$container = jQuery('<div class="cloud-container"></div>')
                             .addClass(me.options.containerExtraClass)
                             .css(me.options.containerStyle).css(consts.containerStyle);
@@ -155,49 +156,49 @@ var Cloud = function(element, options){
             me.containerParent = me.$containerParent.get(0);
             me.$containerInner =jQuery('<div class="cloud-inner"></div>').css(consts.containerInnerStyle);
             me.containerInner = me.$containerInner.get(0);
-            // api refs
+            // API refs
             me.container[fieldKey] = me.api;
             me.$container.data(dataKey, me.api);
-            // place content
+            // Place content
             me.$container.html(me.$containerInner.html(me.$element));
-            // events
+            // Events
             me.$container.on('click.cloud', me.cloudClick)
         },
         destroyContainer: function() {
-            // put element in original place
+            // Put element in original place
             if(me.$elementPrev.length && me.$elementPrev.parent().get(0) == me.elementParent)
                 me.$elementPrev.after(me.$element);
             else
                 me.$elementParent.append(me.$element);
-            // destroy events
+            // Destroy events
             me.$container.off('.cloud');
-            // destroy container
+            // Destroy container
             me.$container.remove();
         },
         destroy: function() {
             me.destroyContainer();
-            // clean all databases
+            // Clean all databases
             deletedCloudIds.push(me.id);
             delete clouds[me.id];
             delete openClouds[me.id];
-            // clean references
+            // Clean references
             me.$element.data(dataKey, null);
             me.element[fieldKey] = null;
             me = null;  // break reference == delete object
         },
 
-        // size & position
+        // Size & position
         autoSize: function() {
             // This need to be executed under simulateOpen
             me.$container.css('height', me.$containerInner.outerHeight());
             me.$container.css('width', me.$containerInner.outerWidth());
         },
         autoPosition: function () {
-            // when positioning is manual, leave
-            if (me.options.positionManually)
+            // When sticking is manual, don't auto position element so that it sticks. Simply leave.
+            if (me.options.stickManually)
                 return;
 
-            // get necessary data
+            // Get necessary data
             var orientation = me.options.orientation,
                 containerHeight = me.$container.outerHeight(),
                 containerWidth = me.$container.outerWidth(),
@@ -207,7 +208,7 @@ var Cloud = function(element, options){
                 // initial offset -  to be changed basing on orientation
                 styles = { 'top': stickToOffset.top, 'left': stickToOffset.left };
 
-            // parse orientation
+            // Parse orientation
             if(!(/(auto|((top|bottom|auto)\-(left|center|right))|((left|right)\-(top|center|bottom)))/).test(orientation))
                 orientation = 'auto';
             if(orientation == 'auto' || orientation == 'auto-auto')
@@ -250,7 +251,7 @@ var Cloud = function(element, options){
                 - (subOr === 'bottom' ? -1 : 1) * me.options.distance[1]);
             }
 
-            // user orientation to add container dedicated class
+            // User orientation to add container dedicated class
             me.$container.addClass([or, 'sub-'+subOr].join(' '));
 
             // If container is not positioned to body,
@@ -262,16 +263,16 @@ var Cloud = function(element, options){
                 styles.left -= parentOffsetDiff.left;
             }
 
-            // save
+            // Save
             me.$container.css(styles);
         },
 
-        // events
+        // Events
         cloudClick: function(event) {
             me.options.cloudClick.apply(this, [me.element, me.api, event]);
         },
 
-        // helpers
+        // Helpers
         simulateOpen: function(func) {
             if(me.isOpen)
                 return func();
@@ -292,7 +293,7 @@ var Cloud = function(element, options){
             return options;
         },
 
-        // actions
+        // Actions
         open: function(event) {
             me.$openEvent = event instanceof jQuery.Event ? event : null;
             me.openEvent = event ? (event.originalEvent || event) : null;
@@ -310,12 +311,58 @@ var Cloud = function(element, options){
             delete openClouds[me.id];
         },
 
-        // api
+        // API
         api: {
+            /**
+             * Info whether cloud is open or closed.
+             *
+             * @memberOf Cloud
+             * @instance
+             * @returns {boolean} State of cloud, true if open
+             */
             isOpen: function() {return me.isOpen},
+
+            /**
+             * Accessor of element wrapped by cloud.
+             *
+             * @memberOf Cloud
+             * @instance
+             * @returns {Element} Element being wrapped by cloud.
+             */
             getElement: function(){ return me.element},
+
+            /**
+             * Opens cloud.
+             *
+             * @memberOf Cloud
+             * @param {Event|jQuery.Event} [event] Event that triggered opening. It will be stored. Passing it may be
+             * useful in some situations.
+             */
             open: function(event) {me.open(event)},
+
+            /**
+             * Accessor of event passed (if any) when opening cloud. Used rarely.
+             *
+             * @memberOf Cloud
+             * @instance
+             * @returns {Event} Event passed when opening cloud.
+             */
+            getOpenEvent: function() {return me.openEvent},
+
+            /**
+             * Closes cloud.
+             *
+             * @memberOf Cloud
+             * @instance
+             */
             close: function() {me.close()},
+
+            /**
+             * Destroys cloud including created wrapper.
+             *
+             * @memberOf Cloud
+             * @instance
+             */
             destroy: function() {me.destroy()}
         }
     };
@@ -324,14 +371,40 @@ var Cloud = function(element, options){
     return me.api;
 };
 
+    /**
+     * @namespace Cloud.skins
+     */
 Cloud.skins = {
+    /**
+     * Set new or override skin.
+     *
+     * @function Cloud.skins.set
+     * @param {String} name Name of the skin.
+     * @param {Object} options Set of options this skin defines. All options as in {@link Cloud} are available,
+     * except skin itself.
+     */
     'set': function(name, options) {
         skins[name] = options;
         delete skins[name].skin;
     },
+
+    /**
+     * Get skin of given name.
+     *
+     * @function Cloud.skins.get
+     * @param {String} name Name of the skin.
+     * @returns {Object} Options object.
+     */
     'get': function(name) {
         return skins[name];
     },
+
+    /**
+     * Get map of all skins.
+     *
+     * @function Cloud.skins.getAll
+     * @returns {Object} Map name-options of all skins.
+     */
     'getAll':function() {
         return skins
     }
@@ -363,5 +436,60 @@ var hookHooks = function() {
 
 window.Cloud = Cloud;
 jQuery(hookHooks);
+
+
+jQuery.fn.extend({
+    /**
+     * @function cloud
+     * @memberOf external:jQuery
+     * @instance
+     *
+     * @param {'create'|'destroy'} cmd Create or destroy Cloud wraps for each of elements of jQuery.
+     * @param {Object} [options] Options as for {@link Cloud}.
+     * @returns {jQuery}
+     */
+
+    cloud: function(cmd, options) {
+        options = typeof options !== 'undefined' ? options : {};
+        var $this = this;
+        if(!$this.length)
+            return $this;
+
+        var eachExecute;
+        switch(cmd) {
+            case 'create':
+                eachExecute = function(index, element){
+                    Cloud(element, options);
+                };
+                break;
+            case 'destroy':
+                eachExecute = function(index, element){
+                    if(jQuery(element).data(dataKey)) {
+                        jQuery(element).data(dataKey).destroy();
+                    }
+                };
+                break;
+            default:
+                eachExecute = function(){};
+                break;
+        }
+
+        $this.each(eachExecute);
+        return $this;
+    }
+});
+
+jQuery.extend({
+    /**
+     * @member cloud
+     * @memberOf external:jQuery
+     * @static
+     *
+     * @property {Object} cloud.skins Alias of {@link Cloud.skins}
+     */
+    cloud: {
+        'skins': Cloud.skins
+    }
+});
 
 })(jQuery);
